eb33445605ec6fa6948957b1528259c0cfe6342b
Witeido17 <hildamiguel02@gmail.com>
2025-10-24 14:58:38 -0300

    Add files via upload

---
+++ oxi.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (request, response) => {
+    response.send('Envie os parâmetros n1 e n2 para a rotas')
+})
+
+app.get('/soma', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) + parseFloat(req.query.n2)
+    if (isNaN(result)) {
+        result = "Valores inválidos."
+    } else {
+        result = `${req.query.n1} + ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+
+app.get('/subtracao', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) + parseFloat(req.query.n2)
+    if (isNaN(result)) {
+        result = "Valores inválidos."
+    } else {
+        result = `${req.query.n1} - ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+
+app.get('/multiplicao', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) + parseFloat(req.query.n2)
+    if (isNaN(result)) {
+        result = "Valores inválidos."
+    } else {
+        result = `${req.query.n1} * ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+
+app.get('/divisao', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) + parseFloat(req.query.n2)
+    if (isNaN(result)) {
+        result = "Valores inválidos."
+    } if (n2 = 0){ 
+        result = "Não é possível dividir por zero."
+    } else {
+        result = `${req.query.n1} / ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+
+app.listen(8080)
